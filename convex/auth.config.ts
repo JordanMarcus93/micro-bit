4561f3e6a32302f493f5f00a0b23c0c36af09239
JordanMarcus93 <jordanmarcusfontaine@gmail.com>
2025-01-02 23:32:28 +0000

    setup convex and generated schemas for users, snippets, stars and comments

---
+++ convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+    providers: [
+      {
+        domain: "https://quick-pangolin-15.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ convex/schema.ts
@@ -0,0 +1,45 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  users: defineTable({
+    userId: v.string(), // clerkid
+    email: v.string(),
+    name: v.string(),
+    isPro: v.boolean(),
+    proSince: v.optional(v.number()),
+    lemonSqueezyCustomerId: v.optional(v.string()),
+    lemonSqueezyOrderId: v.optional(v.string()),
+  }).index("by_user_id", ["userId"]), // fetch user by_user_id, by passing userId
+
+  codeExecutions: defineTable({
+    userId: v.string(),
+    language: v.string(),
+    code: v.string(),
+    output: v.optional(v.string()),
+    error: v.optional(v.string()),
+  }).index("by_user_id", ["userId"]), // fetch user by_user_id, by passing userId
+
+  snippets: defineTable({
+    userId: v.string(),
+    title: v.string(),
+    language: v.string(),
+    code: v.string(),
+    userName: v.string(), // store user's name for easy access
+  }).index("by_user_id", ["userId"]), // fetch user by_user_id, by passing userId
+
+  snippetComments: defineTable({
+    snippetId: v.id("snippets"),
+    userId: v.string(),
+    userName: v.string(),
+    content: v.string(), // HTML content
+  }).index("by_snippet_id", ["snippetId"]),
+
+  star: defineTable({
+    userId: v.id("users"),
+    snippetId: v.id("snippets"),
+  })
+    .index("by_user_id", ["userId"])
+    .index("by_snippet_id", ["snippetId"])
+    .index("by_user_and_snippet_id", ["userId", "snippetId"]),
+});
